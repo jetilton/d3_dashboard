1ee78f7fc8d1c63f21b828d1503a8556c0f4a031
jetilton <jfftilton@gmail.com>
2019-01-25 15:14:13 -0800

    added cwms route

--- app/map/handlers.py
+++ app/map/handlers.py
@@ -7,7 +7,49 @@ from flask import abort
 import json
 import requests
 from app.map import bp
+from flask_restful import reqparse
+from datetime import datetime
+from flask import jsonify
 
+
+def time_window_url(paths, public=True, lookback = 7, start_date = False, end_date = False, timezone = 'PST'):
+    """
+    helper function for cwms_read
+    
+    Arguments:  
+        
+        path -- cwms data path, 
+        public -- boolean, 
+        start_date -- date integer tuple format (YYYY, m, d)
+        end_date -- date integer tuple format (YYYY, m, d)
+        timezone -- optional keyword argument if time zone is specified.  
+                    Defaults to 'PST' if nothing set
+    Returns:
+        
+        url -- url string of CWMS data webservice for the specified 
+               data path and time window
+               
+    """
+
+    if isinstance(paths, list): 
+        path = '%22%2C%22'.join(paths)
+    else: path = paths
+        
+    if public:
+        url = r'http://pweb.crohms.org/dd/common/web_service/webexec/getjson?timezone=TIMEZONE_&query=%5B%22PATH%22%5D&'
+    else:
+        url = r'http://nwp-wmlocal2.nwp.usace.army.mil/common/web_service/webexec/getjson?timezone=TIMEZONE_&query=%5B%22PATH%22%5D&'
+    
+    url = url.replace('PATH', path).replace('TIMEZONE_', timezone)
+    if lookback:
+        time = 'backward=' + str(lookback) + 'd'
+        url = url + time
+    else:
+        url = url + 'startdate=START_MONTH%2FSTART_DAY%2FSTART_YEAR+00%3A00&enddate=END_MONTH%2FEND_DAY%2FEND_YEAR+23%3A00'
+        url = url.replace('START_MONTH', str(start_date.month)).replace('START_DAY', str(start_date.day)).replace('START_YEAR', str(start_date.year))
+        url = url.replace('END_MONTH', str(end_date.month)).replace('END_DAY', str(end_date.day)).replace('END_YEAR', str(end_date.year))
+    
+    return url
 
 
 @bp.route('/')
@@ -100,7 +142,6 @@ def edit_cbt(cbt):
                 form.__dict__[form_rows[index][1]].data = parameter
             form.__dict__[form_rows[index][2]].data = path
             
-    
     return render_template('map/edit_cbt.html', cbt=cbt, form=form,form_rows=form.form_rows)
 
 @bp.route('/<cbt>/modal',methods = ['GET'])
@@ -108,3 +149,37 @@ def cbt_modal(cbt):
     cbt = Cbt.query.filter_by(cbt=cbt.upper()).first()
     paths = Paths.query.filter_by(cbt = cbt.cbt)
     return render_template('map/cbt_modal.html', cbt = cbt.cbt, title = cbt.name, paths = paths)
+
+@bp.route('/cwms', methods = ['GET', 'POST'])
+def cwms():
+    parser = reqparse.RequestParser()
+    parser.add_argument('paths', action='append')
+    parser.add_argument('lookback', type=int)
+    parser.add_argument('startdate', type=lambda x: datetime.strptime(x,'%Y-%m-%d') )
+    parser.add_argument('enddate', type=lambda x: datetime.strptime(x,'%Y-%m-%d') )
+    args = parser.parse_args()
+    paths = args['paths']
+    lookback = args['lookback']
+    if lookback:
+        start_date = False
+        end_date = False
+    else:
+        start_date = args['startdate']
+        end_date = args['enddate']
+        lookback = False
+    url = time_window_url(paths, public=True, lookback = lookback, start_date = start_date, end_date = end_date, timezone = 'PST')
+    requests.packages.urllib3.disable_warnings() 
+    data = json.loads(requests.get(url, verify = False).text)
+    data_list = []
+    for key,value in data.items():
+        value['cbt'] = key
+        pathname = list(value['timeseries'])[0]
+        value['pathname'] = pathname
+        vals = value['timeseries'][pathname]['values']
+        ts_list = []
+        for val in vals:
+            ts_list.append({'date':val[0],'value':val[1],'flag':val[2]})
+        value['timeseries'] = value['timeseries'][pathname]
+        value['timeseries']['values'] = ts_list
+        data_list.append(value)
+    return jsonify(data_list)
