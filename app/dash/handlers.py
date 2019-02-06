3304dcd71c6cbd89f58ae8ae7f1ee8b03c5169d8
jetilton <jfftilton@gmail.com>
2019-02-06 08:54:59 -0800

    reorganized static files

--- app/dash/handlers.py
+++ app/dash/handlers.py
@@ -1,12 +1,10 @@
 from app import db
 from flask import render_template, url_for, flash, redirect, request
-from app.map.forms import CbtForm, PathsForm, add_paths
-from flask_login import login_required
-from app.models import Cbt, Paths
-from flask import abort
-import json
-import requests
+from app.models import Cbt
+
+
 from app.dash import bp
+from cwms_read.cwms_read import get_cwms
 
 
 
@@ -14,4 +12,11 @@ from app.dash import bp
 
 @bp.route('/<cbt>/dash', methods = ['GET'])
 def dash_cbt(cbt):
-        return render_template('dash/dash.html')
+    return render_template('dash/dash.html')
+    
+    
+@bp.route('/hydrographdata/<cbt>', methods = ['GET'])
+def dash_data(cbt):
+    cbt = Cbt.query.filter_by(cbt=cbt).first()
+    
+    return render_template('dash/dash.html')
